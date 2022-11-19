74c84eefd7347498dca63720ddbf6e52e26808ec
lucasrodrigges <rodriggeslucas@gmail.com>
2022-11-19 03:59:16 -0300

    adicionando modificações de CSS

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/ContactForm.css';
 
 export default function ContactForm() {
+  const [msg, setMsg] = useState({
+    firstName: '',
+    lastName: '',
+    messageTitle: '',
+    messageBody: '',
+  });
+  const [isDisabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const values = Object.values(msg);
+
+    if (values.every((el) => el)) setDisabled(false);
+    else setDisabled(true);
+  }, [msg]);
+
+  function handleChange({ target: { name, value } }) {
+    setMsg({
+      ...msg,
+      [name]: value,
+    });
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
   }
@@ -14,11 +36,11 @@ export default function ContactForm() {
         onSubmit={handleSubmit}
       >
         <h2>Contact</h2>
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Message Title" />
-        <textarea name="" id="" placeholder="Your Message" />
-        <button className="button-1" type="submit">Send</button>
+        <input name="firstName" type="text" placeholder="First Name" onChange={handleChange} />
+        <input name="lastName" type="text" placeholder="Last Name" onChange={handleChange} />
+        <input name="messageTitle" type="text" placeholder="Message Title" onChange={handleChange} />
+        <textarea name="messageBody" id="" placeholder="Your Message" onChange={handleChange} />
+        <button className="button-1" disabled={isDisabled} type="submit">Send</button>
       </form>
     </section>
   );
